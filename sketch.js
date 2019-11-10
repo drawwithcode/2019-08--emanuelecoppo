bc5ddf884a7922f1bd8b688c82905602709a7120
emanuelecoppo <emanuelecoppo@gmail.com>
2019-11-10 17:46:57 +0100

    Token

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@ var position;
 var date, hours, minute, seconds;
 var latitude, logitude, timestamp;
 var myLat, myLng;
-var token = "pk.eyJ1IjoiZW1hbnVlbGVjb3BwbyIsImEiOiJjazJ0M2RnOW4xM2l1M2JsNDNqdXlneDRlIn0.547Nm9aZd0mz6TX4KbJtdw"
+// var token = "pk.eyJ1IjoiZW1hbnVlbGVjb3BwbyIsImEiOiJjazJ0M2RnOW4xM2l1M2JsNDNqdXlneDRlIn0.547Nm9aZd0mz6TX4KbJtdw"
+var token = "pk.eyJ1IjoiZW1hbnVlbGVjb3BwbyIsImEiOiJjazJ0OGJkNTcwc2MzM2dvMzc4cmFpZzhvIn0.oUzcEgUg6rIwYe6tmALbLg"
 
 var myMap;
 var mappa = new Mappa("MapboxGL", token);
@@ -20,12 +21,12 @@ function setup() {
   var options = {
     //https://docs.mapbox.com/mapbox-gl-js/api/#map
     //https://docs.mapbox.com/api/maps/#styles
-    style: "mapbox://styles/emanuelecoppo/ck2t5l32v0s421cs0amhz6ggk",
     lat: myLat,
     lng: myLng,
     zoom: 6,
     minZoom: 0,
-    pitch: 0,
+    style: "mapbox://styles/mapbox/light-v10",
+    pitch: 0
   }
 
   myMap = mappa.tileMap(options);
